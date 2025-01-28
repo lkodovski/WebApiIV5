cfa7b84b6b2a69d4b90e4071fb09809e3f052e75
lkodovski <l.kodovski@hotmail.com>
2025-01-28 15:38:21 +0100

    Prv commit

---
+++ ApiMovies/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiMovies.Models
+{
+    public class Category
+    {
+        //Ovde se dodavaat svojstvata
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+    }
+}

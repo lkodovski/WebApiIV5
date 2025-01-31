0ef46790a6ff6ae951aa4619ebb16a87413f626a
lkodovski <l.kodovski@hotmail.com>
2025-01-31 11:24:22 +0100

    Podesuvanje na DbContext

--- ApiMovies/Program.cs
+++ ApiMovies/Program.cs
@@ -1,6 +1,14 @@
+
+
+using ApiMovies.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionSql")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

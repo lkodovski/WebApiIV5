08b0ab03bb37e62bbb62cf5db8f869fe7b0cac2c
lkodovski <l.kodovski@hotmail.com>
2025-02-14 07:42:53 +0100

    Repo

---
+++ ApiMovies/Repository/CategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using ApiMovies.Models;
+using ApiMovies.Repository.IRepository;
+
+namespace ApiMovies.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public bool CreateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExistCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ExistCategory(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetCategory(int CategoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ApiMovies/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApiMovies.Models;
+
+namespace ApiMovies.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+
+        Category GetCategory(int CategoryId);
+
+        bool ExistCategory(int id);
+        bool ExistCategory(string name);
+        bool CreateCategory(Category category);
+        bool UpdateCategory(Category category);
+        bool DeleteCategory(Category category);
+        bool Save();
+    }
+}
